83ac4e23a7a90036ca279c1249733712628b2d68
BrunoH300 <116306479+BrunoH300@users.noreply.github.com>
2023-09-12 20:06:04 -0300

    Update listaExercício01.sql
    commit 3

--- listaExercício01.sql
+++ listaExercício01.sql
@@ -4,3 +4,9 @@ Questao 1{
 Questao 2{
   SELECT nome FROM autores WHERE YEAR(nascimento) < 1900;
 }
+Questao 3{
+  SELECT livros.titulo
+  FROM livros
+  INNER JOIN autores ON livros.autor_id = autores.id
+  WHERE autores.nome = 'J.K. Rowling';
+}
