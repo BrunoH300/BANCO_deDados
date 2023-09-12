291341ca0c51e7fb42cab066d067f56e6be7aa17
BrunoH300 <116306479+BrunoH300@users.noreply.github.com>
2023-09-12 20:27:07 -0300

    Update listaExercício01.sql
    commit 19

--- listaExercício01.sql
+++ listaExercício01.sql
@@ -105,7 +105,12 @@ Questao 18{
   LEFT JOIN vendas ON livros.id = vendas.livro_id
   GROUP BY autores.nome;
 }
-
+Questao 19{
+  SELECT alunos.nome AS aluno, COUNT(matriculas.id) AS total_de_matriculas
+  FROM alunos
+  LEFT JOIN matriculas ON alunos.id = matriculas.aluno_id
+  GROUP BY alunos.nome;
+}
 
 
 
